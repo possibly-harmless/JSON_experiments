546ec8fdcae28ebd789432d36f685bccacf94111
Leonid Shifrin <73906510+possibly-harmless@users.noreply.github.com>
2021-08-17 00:28:30 +0000

    Refactored to orthogonal functions. Factored out db mocking logic. Removed old / dead code.

--- main.c
+++ main.c
@@ -8,85 +8,81 @@
 
 #define SUCCESS 0
 #define FAILURE 1
+#define BOOL int
+
 
 char *str_dup (const char* src);
-char **str_array_copy(char **src, size_t size);
+int json_serialization_callback(void *data, int argc, char **argv, char **azColName);
 
-static int callback(void *data, int argc, char **argv, char **azColName);
+JSON_Status serialize_to_json(
+  JSON_Status exec(
+    void * exec_data,
+    int cb(void * , int , char **, char **)
+  ),
+  char **result_ptr
+);
+
+JSON_Status execute_mock_db(void * exec_data,int cb(void * , int , char **, char **));
+
 
 int main(void) {
+  char *result = NULL;
 
-  char *colnames[] = {"ID", "NAME", "AGE", "ADDRESS", "SALARY"};
-
-  char *test_array[][5] = {
-    {"1", "Paul", "32", "California", "20000.00"},
-    {"2", "Allen", "25", "Texas", "15000.00" },
-    {"3", "Teddy", "23", "Norway", "20000.00" },
-    {"4", "Mark", "25", "Rich-Mond", "65000.00"}
-  };
-
-  char *serialized_string = NULL;
-  JSON_Status status = JSONFailure;
-  size_t  len;
-  char **str_vals;
-  
-  JSON_Value * root_val = json_value_init_object();
-  JSON_Object * main_obj = json_value_get_object(root_val);
-
-
-
-
-  // JSON_Value * root_val = json_value_init_array();
-  // JSON_Array * main_array =  json_value_get_array(root_val);
-
-  // len = sizeof(test_array) / sizeof(char *);
-
-  // if(!(str_vals = str_array_copy(test_array, len))){
-  //   return EXIT_FAILURE;
-  // }
-
-  // for(int i = 0; i < len; i++){
-  //   status = json_array_append_string(main_array, str_vals[i]);
-  //   if(status != JSONSuccess){
-  //     break;
-  //   }
-  // }
-
-  // // Serialization
-  // if(status == JSONSuccess){
-  //   serialized_string = json_serialize_to_string_pretty(root_val);
-  //   puts(serialized_string);
-  //   json_free_serialized_string(serialized_string);
-  // } else {
-  //   puts("JSON array appending error");
-  // }
-
-  // len = json_array_get_count(main_array);
-
-  // for(int i = 0; i < len; i++){
-  //   const char* elem = json_array_get_string(main_array, i);
-  //   if(!elem){
-  //     // Handle internal error, we should not have NULL pointers
-  //     continue; // For now
-  //   }
-  //   // NOTE: don't need to call <free> explicitly, since json_value_free()
-  //   // call takes care of this. This is b.t.w. not a very consistent design
-  //   // on parson's side.
-  //   //
-  //   // free((void *) elem);
-  // }   
-
-  json_value_free(root_val); 
+  if(serialize_to_json(execute_mock_db, &result) == JSONSuccess){
+    puts(result);
+    free(result);
+  } else {
+    puts("JSON serialization failed");
+  }
 
   return 0;
 }
 
-static int callback(void *data, int argc, char **argv, char **azColName){
+  
+JSON_Status serialize_to_json(
+  JSON_Status exec(
+    void * exec_data,
+    int cb(void * , int , char **, char **)
+  ),
+  char **result_ptr
+){
+  char *serialized_string = NULL;
+  JSON_Status status = JSONFailure;
 
+  JSON_Value * root_val = json_value_init_object();
+  JSON_Object * main_obj = json_value_get_object(root_val);
+
+  status = exec((void *)main_obj, json_serialization_callback);
+
+  if(status == JSONSuccess){
+    serialized_string = json_serialize_to_string_pretty(root_val);
+    if(serialized_string){
+      *result_ptr = str_dup(serialized_string);
+      json_free_serialized_string(serialized_string);
+    } else {
+      status = JSONFailure;
+      *result_ptr = NULL;
+    }
+  }
+  // This seems to also free any strings in string arrays, keys, etc.
+  // TODO: double-check that!
+  json_value_free(root_val); 
+  return status;
+}
+
+
+int json_serialization_callback(void *data, int argc, char **argv, char **azColName){
   // TODO: improve sloppy resource / memory management
-
   JSON_Object * obj = (JSON_Object *) data;
-  static int first_run = TRUE;
+  static BOOL first_run = TRUE;
+
+  // TODO : temporary, until we either start throwing exceptions from  callback, or
+  // switch to using the step interface
+  static BOOL failed = FALSE; 
+
+  if(failed){
+    return FAILURE;
+  }
 
   for(int i = 0; i < argc; i++){
     const char* col_name = azColName[i];
@@ -96,28 +92,57 @@ static int callback(void *data, int argc, char **argv, char **azColName){
       char *key = str_dup(col_name);
       JSON_Value * col_values = json_value_init_array();
       if (!col_values){
+        failed = TRUE;
         return FAILURE;
       }
       status = json_object_set_value(obj, key, col_values);
       if(status != JSONSuccess){
+        failed = TRUE;
         return FAILURE;
       }
     } 
     JSON_Array * cvals = json_object_get_array(obj, col_name);
     if(!cvals){
       // Missing column should be considered a failure
+      failed = TRUE;
       return FAILURE;
     }
     // Append column value for this row
     char * val_copy = str_dup(argv[i]);
     status = json_array_append_string(cvals, val_copy);
-    if(!status){
+    if(status != JSONSuccess){
       free(val_copy);
+      failed = TRUE;
       return FAILURE;
     }
   }
   first_run = FALSE;
   return SUCCESS;
+}
+
+
+JSON_Status execute_mock_db(void * exec_data,int cb(void * , int , char **, char **)){
+  char *colnames[] = {"ID", "NAME", "AGE", "ADDRESS", "SALARY"};
+
+  char *test_array[][5] = {
+    {"1", "Paul", "32", "California", "20000.00"},
+    {"2", "Allen", "25", "Texas", "15000.00" },
+    {"3", "Teddy", "23", "Norway", "20000.00" },
+    {"4", "Mark", "25", "RichMond", "65000.00"}
+  };
+
+  size_t  
+    n_rows = sizeof(test_array) / sizeof(test_array[0]), 
+    n_cols = sizeof(test_array[0]) / sizeof(test_array[0][0]);
+
+  for(int i = 0; i < n_rows; i++){
+    if(cb(exec_data, n_cols, test_array[i], colnames) != SUCCESS){
+      puts("Error encountered in callback. Terminating...");
+      return JSONFailure;
+    }
+  }
+
+  return JSONSuccess;
 }
 
 
@@ -129,21 +154,4 @@ char *str_dup (const char* src){
   while((*p++ = *src++))
     ; 
   return result;
-}
-
-char **str_array_copy(char **src, size_t size){
-  char **copy = NULL;
-  copy = (char**) malloc(size * sizeof(char *));
-  if(!copy){
-    return NULL;
-  }
-  for(int i = 0; i < size; i++){
-    char *current = str_dup(src[i]);
-    if(!current){
-      free(copy);
-      return NULL;
-    }
-    copy[i] = current;
-  }  
-  return copy;
 }
